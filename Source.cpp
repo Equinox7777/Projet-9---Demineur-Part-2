2aac5967cc8b4b0b983675ab8b43647ffb8f19b5
lviennet <lviennet@gaming.tech>
2023-03-02 11:05:38 +0100

    SDL2 instalé

---
+++ Source.cpp
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "SDL.h"
+#undef main
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("Demineur", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // Dessinez la grille de cases ici
+
+    SDL_Event event;
+    bool quit = false;
+
+    while (!quit)
+    {
+        while (SDL_PollEvent(&event) != 0)
+        {
+            if (event.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+
+            // Gérez les événements de la souris ici
+        }
+
+        // Mettez à jour la grille de cases ici
+
+        SDL_RenderClear(renderer);
+
+        // Dessinez la grille de cases mise à jour ici
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
